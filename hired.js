fa2ccbcacb24ea4b0cde5fc5479ad26a892ee318
gdknas <mckevitt.r@gmail.com>
2023-09-08 14:55:32 -0500

    added option to hire ryan for k/yr

---
+++ hired.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hireButton = document.getElementById("hireButton");
+
+  hireButton.addEventListener("click", function (e) {
+    e.preventDefault(); // Prevent the default behavior of the anchor tag
+    alert("You've just hired Ryan for a starting salary of $30k/yr.");
+  });
+});

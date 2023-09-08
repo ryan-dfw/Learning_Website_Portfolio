e1b134dc24622c2a7cec30d824cf123e4eaced6d
gdknas <mckevitt.r@gmail.com>
2023-09-08 15:00:34 -0500

    added an image and description to about page

--- hired.js
+++ hired.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const hireButton = document.getElementById("hireButton");
 
   hireButton.addEventListener("click", function (e) {
-    e.preventDefault(); // Prevent the default behavior of the anchor tag
+    e.preventDefault();
     alert("You've just hired Ryan for a starting salary of $30k/yr.");
   });
 });
